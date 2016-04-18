75d2eaa2f67f6491e979121fb3a03ee64dd2f67d
njpc875 <njpc875@gmail.com>
2016-04-18 12:21:21 +0100

    Gun Controls
    Added a control script for a gun attached to the player's ship, right
    now it only controls the direction that it is pointing

---
+++ GameJam/Assets/Scripts/gunControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunControl : MonoBehaviour {
+
+
+	//strings for the axes
+	public string vertAxis;
+	public string horizAxis;
+	public string shootAxis;
+
+	
+	private Vector3 direction;//direction that the gun points
+	private Quaternion rotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gunDirection ();
+	}
+
+	void gunDirection()
+	{
+		direction = new Vector3(Input.GetAxis (horizAxis), Input.GetAxis (vertAxis), 0);
+		rotation = Quaternion.LookRotation (direction, Vector3.up);
+		transform.rotation = rotation;
+	}
+}
---
+++ GameJam/Assets/Scripts/jetControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jetControl : MonoBehaviour {
+
+	//strings for the axes
+	public string vertAxis;
+	public string horizAxis;
+	public string powerAxis;
+	public float maxSpeed = 20f;
+
+	private Vector3 direction;//direction that the jet points
+	private float power;
+	private Quaternion rotation;
+	public float powerModifier = 1f;//used to increase or decrease the power of the jet
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		jetDirection ();
+		jetPower ();
+		gameObject.transform.rotation = rotation;
+		//gameObject.GetComponent<Rigidbody>().velocity += (direction * power);
+		gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * Time.deltaTime * power);
+
+		//makes sure that the jet never moves off its axis
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+	}
+
+	void jetDirection()
+	{
+		
+		/*direction = new Vector3(Input.GetAxis (horizAxis), Input.GetAxis (vertAxis), 0);
+
+		Vector3 axis = Vector3.Cross(gameObject.transform.forward, direction);
+		rotation = Vector3.Angle(gameObject.transform.forward, direction) / 180.0f * (axis.y < 0 ? -1 : 1);
+		transform.rotation = rotation;*/
+		Transform mainCamera = Camera.main.transform;
+		direction = new Vector3(Input.GetAxis (horizAxis), Input.GetAxis (vertAxis), 0);
+		rotation = Quaternion.LookRotation (direction, Vector3.up);
+		transform.rotation = rotation;
+	}
+
+	void jetPower()
+	{
+		power = (Input.GetAxis (powerAxis)) * powerModifier;
+		Debug.Log(power);
+	}
+}
