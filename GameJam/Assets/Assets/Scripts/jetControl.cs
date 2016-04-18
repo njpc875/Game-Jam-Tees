a53fee9ffc6d1099d633d6ac5f42df9310145c45
njpc875 <njpc875@gmail.com>
2016-04-18 11:05:35 +0100

    Added Unity Project
    Added in Unity project with script for ship control.

---
+++ GameJam/Assets/Assets/Scripts/jetControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jetControl : MonoBehaviour {
+
+	//strings for the axes
+	public string vertAxis;
+	public string horizAxis;
+	public string powerAxis;
+	public float maxSpeed = 20f;
+
+	private Vector3 direction;//direction that the jet points
+	private float power;
+	private Quaternion rotation;
+	public float powerModifier = 1f;//used to increase or decrease the power of the jet
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		jetDirection ();
+		jetPower ();
+		gameObject.transform.rotation = rotation;
+		//gameObject.GetComponent<Rigidbody>().velocity += (direction * power);
+		gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * Time.deltaTime * power);
+
+		//makes sure that the jet never moves off its axis
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+	}
+
+	void jetDirection()
+	{
+		direction = new Vector3(Input.GetAxis (horizAxis), Input.GetAxis (vertAxis), 0);
+		rotation = Quaternion.LookRotation (direction, Vector3.up);
+		transform.rotation = rotation;
+	}
+
+	void jetPower()
+	{
+		power = (Input.GetAxis (powerAxis)) * powerModifier;
+		Debug.Log(power);
+	}
+}
---
+++ GameJam/Assets/Assets/Scripts/shipController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shipController : MonoBehaviour 
+{
+    public GameObject shipBody;
+    public Rigidbody shipRigidbody;
+
+    private float rotationSpeed = 2.5f;
+    private float moveSpeed = 0.5f;
+    private float maxSpeed = 20f;
+    private Vector3 direction;
+
+    private bool leftPress = false;
+    private bool rightPress = false;
+    private bool spacePress = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // Left key handler.
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Left Down");
+            leftPress = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            Debug.Log("Left Up");
+            leftPress = false;
+        }
+
+        // Right key handler.
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Right Down");
+            rightPress = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            Debug.Log("Right Up");
+            rightPress = false;
+        }
+
+        // Spacebar handler.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Down");
+            spacePress = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Space Up");
+            spacePress = false;
+        }
+
+        // Left key held down, rotate anti-clockwise.
+        if(leftPress)
+        {
+            shipBody.transform.Rotate(rotationSpeed * new Vector3(0, 0, 1));
+        }
+
+        // Right key held down, rotate clockwise.
+        if(rightPress)
+        {
+            shipBody.transform.Rotate(rotationSpeed * new Vector3(0, 0, -1));
+        }
+
+        // Get the up direction in local coordinates.
+        direction = shipBody.transform.up;
+
+        // Spacebar held down, generate thrust in the up direction.
+        if(spacePress)
+        {
+            shipRigidbody.velocity += (direction * moveSpeed);
+        }
+
+        // Clamp the maximum velocity.
+        shipRigidbody.velocity = Vector3.ClampMagnitude(shipRigidbody.velocity, maxSpeed);
+	}
+}
